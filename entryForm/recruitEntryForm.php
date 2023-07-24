88dd5693ba7052f986f206ee056455df51fca2d9
Zak khan <zakaria2012@gmail.com>
2023-07-24 18:48:59 +0100

    added required to input form

--- entryForm/recruitEntryForm.php
+++ entryForm/recruitEntryForm.php
@@ -11,17 +11,17 @@
         <form class='recruiters' action="../inc/recrinsert.php" method="POST">
             <div class="mb-3 col-6">
                 <label for="recruiter_name" class="form-label">Recruiter Name</label>
-                <input type="text" name='recruiter_name' class="form-control" id="recruiter_name" aria-describedby="Recruiters name">
+                <input type="text" name='recruiter_name' class="form-control" id="recruiter_name" aria-describedby="Recruiters name" required>
                 <div id="recruiter_name" class="form-text"></div>
             </div>
             <div class="mb-6 col-6">
                 <label for="website" class="form-label">Job board</label>
-                <input type="text" class="form-control" name='website' id="website" aria-describedby="Enter website url">
+                <input type="text" class="form-control" name='website' id="website" aria-describedby="Enter website url" required>
                 <div id="websiteHelper" class="form-text">Website</div>
             </div>
             <div class="mb-3 col-8">
                 <label for="comments" class="form-label">Comments</label>
-                <textarea type="textarea" name='comments' class="form-control " id="comments"></textarea>
+                <textarea type="textarea" name='comments' class="form-control " id="comments" required></textarea>
             </div>
 
              <input type="submit" name='submit' value='Save' class="btn btn-primary" onclick="clearFrom()"></button>
