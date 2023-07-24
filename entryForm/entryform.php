21f4d2c7513d5d23891cac0ef7efc893b3173e20
Zak khan <zakaria2012@gmail.com>
2023-07-24 18:47:34 +0100

    added required to input form

--- entryForm/entryform.php
+++ entryForm/entryform.php
@@ -9,24 +9,24 @@
         <form action="inc/insertinto.php" method="POST">
             <div class="mb-3 col-6">
                 <label for="exampleInputEmail1" class="form-label">Job title</label>
-                <input type="text" name='job_title' class="form-control" id="exampleInputEmail1" aria-describedby="enter email address">
+                <input  type="text" name='job_title' class="form-control" id="exampleInputEmail1" aria-describedby="enter email address" required>
                 <div id="emailHelp" class="form-text"></div>
             </div>
             <div class="mb-3 col-3">
                 <label for="taxChoice" class="form-label">Taxonomy</label>
-                <select name="status" id="status" list="status taxChoice" name="status" class="form-control"  aria-describedby="Satus application in">
+                <select name="status" id="status" list="status taxChoice" name="status" class="form-control"  aria-describedby="Satus application in" required>
                         <?php  taxonomyResult(); ?>
                 </select>
                 <div id="emailHelp" class="form-text"></div>
             </div>
             <div class="mb-6 col-6">
                 <label for="exampleInputEmail1" class="form-label">Job URL</label>
-                <input type="text" class="form-control" name='url' id="url" aria-describedby="enter email address">
+                <input type="text" class="form-control" name='url' id="url" aria-describedby="enter email address" required>
                 <div id="emailHelp" class="form-text">Job url</div>
             </div>
             <div class="mb-3 col-8">
                 <label for="coverletterno " class="form-label">Cover letter</label>
-                <textarea type="textarea" name='description' class="form-control " id="coverletter"></textarea>
+                <textarea type="textarea" name='description' class="form-control " id="coverletter" required></textarea>
             </div>
 
              <input type="submit" name='submit' value='Add to database' class="btn btn-primary"></button>
